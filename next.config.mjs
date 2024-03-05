24c60fae52ece217216ca3780c0beeb869166922
Igor Veyner <igorveyner95@gmail.com>
2024-03-04 21:30:27 -0800

    feat add sbasic sections

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      // "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
+      // "https://aceternity.com/images/products/thumbnails/new/cursor.png",
+      // "https://aceternity.com/images/products/thumbnails/new/rogue.png",
+      // "https://aceternity.com/images/products/thumbnails/new/editorially.png",
+      // "https://aceternity.com/images/products/thumbnails/new/editrix.png",
+      // "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
+      // "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
+      // "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
+      // "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
+      // "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
+      // "https://aceternity.com/images/products/thumbnails/new/renderwork.png", "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
+      // "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
+      // "https://aceternity.com/images/products/thumbnails/new/invoker.png",
+      // "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
+      { hostname: "aceternity.com" }
+    ]
+  }
+};
 
 export default nextConfig;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,21 @@
-import type { Config } from "tailwindcss";
 
-const config: Config = {
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+const colors = require("tailwindcss/colors");
+const {
+  default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
+
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
+    "./src/**/*.{ts,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     extend: {
       backgroundImage: {
@@ -15,6 +25,35 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [addVariablesForColors],
 };
-export default config;
+
+function addVariablesForColors({ addBase, theme }: any) {
+  let allColors = flattenColorPalette(theme("colors"));
+  let newVars = Object.fromEntries(
+    Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+  );
+
+  addBase({
+    ":root": newVars,
+  });
+}
+
+// const config: Config = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {
+//       backgroundImage: {
+//         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+//         "gradient-conic":
+//           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
+// export default config;
