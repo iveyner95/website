2e1d897fc6d7cad57f7a2b127227b205d32b925d
Igor Veyner <igorveyner95@gmail.com>
2024-03-10 18:45:26 -0700

    chore: remove comment

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { ContactSection } from './components/contact-section';
 import { HeroParallax } from './components/hero-parallax';
 
 export default function Home() {
-  // TODO: hide scrollbar
   return (
     <main>
       <HeroParallax previews={previews} />
