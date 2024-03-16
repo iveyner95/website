11988f3f9aedc43f44b02a55ecf13b2db97a526f
Igor Veyner <igorveyner95@gmail.com>
2024-03-16 11:35:33 -0700

    build: update nextjs build config

--- next.config.mjs
+++ next.config.mjs
@@ -2,23 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    remotePatterns: [
-      // "https://aceternity.com/images/products/thumbnails/new/moonbeam.png",
-      // "https://aceternity.com/images/products/thumbnails/new/cursor.png",
-      // "https://aceternity.com/images/products/thumbnails/new/rogue.png",
-      // "https://aceternity.com/images/products/thumbnails/new/editorially.png",
-      // "https://aceternity.com/images/products/thumbnails/new/editrix.png",
-      // "https://aceternity.com/images/products/thumbnails/new/pixelperfect.png",
-      // "https://aceternity.com/images/products/thumbnails/new/algochurn.png",
-      // "https://aceternity.com/images/products/thumbnails/new/aceternityui.png",
-      // "https://aceternity.com/images/products/thumbnails/new/tailwindmasterkit.png",
-      // "https://aceternity.com/images/products/thumbnails/new/smartbridge.png",
-      // "https://aceternity.com/images/products/thumbnails/new/renderwork.png", "https://aceternity.com/images/products/thumbnails/new/cremedigital.png",
-      // "https://aceternity.com/images/products/thumbnails/new/goldenbellsacademy.png",
-      // "https://aceternity.com/images/products/thumbnails/new/invoker.png",
-      // "https://aceternity.com/images/products/thumbnails/new/efreeinvoice.png",
-      { hostname: "aceternity.com" }
-    ]
+    remotePatterns: []
   }
 };
 
